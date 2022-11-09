20200d58f8acb3e98ed0353bfad4dc9cee31be3e
Frank Liu <frvnkliu@gmail.com>
2022-11-09 00:07:08 -0500

    learning about vue, added modified db.mjs to be implemented on courant

--- db.mjs
+++ db.mjs
@@ -1,15 +1,39 @@
-const mongoose = require('mongoose'),
-	URLSlugs = require('mongoose-url-slugs');
+import mongoose from 'mongoose'
+import mongooseSlugPlugin from 'mongoose-slug-plugin';
+
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/fl2211';
+}
+
+mongoose.connect(dbconf);
 
 const Shape = new mongoose.Schema({
     type: String,
-    pos: {x: Integer, y: Integer},
-    color: {r: Integer, g: Integer, b: Integer}
+    pos: {x: Number, y: Number},
+    color: {r: Number, g: Number, b: Number}
 });
 
 const Card = new mongoose.Schema({
 	name: String,
-	backgroundcolor: {r: Integer, g: Integer, b: Integer},
+	backgroundcolor: {r: Number, g: Number, b: Number},
 	shapes: [Shape]
 });
 
@@ -17,5 +41,5 @@ const User = new mongoose.Schema({
     username: String,
     hash: String,
     Cards: [Card],
-    profilePicIndex: Integer
+    profilePicIndex: Number
 });
