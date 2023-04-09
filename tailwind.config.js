4ca205c74c7b2a30450890c45fd2315b76d51775
Frank Liu <frvnkliu@gmail.com>
2023-04-09 02:04:47 -0400

    Fixed a few bugs, and included tailwind as a file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}"],
+  content: [
+    "./public/*.html", 
+    "./public/scripts/*.js"
+  ],
   theme: {
     extend: {},
   },
